6464cd45f4df96192dc5528bd171a7b9aab941bb
James Courtois <jcourtois@gmail.com>
2023-02-03 11:29:50 -0500

    add test for a is not b and make it pass

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub struct Solution {}
 
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
-        true
+        s == p
     }
 }
 
@@ -14,5 +14,10 @@ mod tests {
     #[test]
     fn a_is_a() {
         assert_eq!(Solution::is_match(String::from("a"), String::from("a")), true);
+    }
+
+    #[test]
+    fn a_is_not_b() {
+        assert_eq!(Solution::is_match(String::from("b"), String::from("a")), false);
     }
 }
